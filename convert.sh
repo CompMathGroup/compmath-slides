f8a489d989463d95696f343d4a9adfd56ba138df
Ivan Tsybulin <tsybulin@crec.mipt.ru>
2016-11-14 13:51:30 +0300

    Enlarged slides

--- convert.sh
+++ convert.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
+
+ver=$(python --version 2>&1 | grep -o Anaconda)
+if [ "x$ver" != "xAnaconda" ]; then
+	echo "Need patched nbconvert" && exit 1
+fi
+
 for path in jupyter-slides/*; do
 	fn=$(echo $path | cut -f 2 -d '/' || $1)
 	num=$(echo $fn | cut -f 1 -d '-')
 	name=$(echo $fn | cut -f 2 -d '-')
+
+	if [ -f $num/.keep ]; then
+		continue;
+	fi
 
 	echo "Processing #$num aka $name"
 	mkdir -p $num
